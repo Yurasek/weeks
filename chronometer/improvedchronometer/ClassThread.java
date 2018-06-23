9a7adde2d06c35ac703d4d3d7fd7758212a1d35e
Yurasek <lyalyatopolya1337@gmail.com>
2018-06-24 01:04:57 +0400

    rewrited chronometer with the third thread, Angliskey esicho ne moyo, soryan)

---
+++ chronometer/improvedchronometer/ClassThread.java
@@ -0,0 +1,55 @@
+package week3.chronometer.improvedchronometer;
+
+import java.util.concurrent.TimeUnit;
+
+import static java.lang.System.out;
+
+
+public class ClassThread implements Runnable {
+
+    public int time = 0;
+
+    public synchronized void changeTime(){
+        time++;
+    }
+    public synchronized int getTime(){
+        return time;
+    }
+    @Override
+    public void run() {
+            for(int i = 0;i < 60;i++) {
+                changeTime();
+                out.println(getTime());
+                try{
+                    TimeUnit.MILLISECONDS.sleep(1000);
+                }catch (InterruptedException e){}
+            }
+    }
+    public static void main(String[] args) {
+        ClassThread seconds = new ClassThread();
+        new Thread(seconds).start();
+        (new Thread(()->{
+            while(seconds.getTime() < 60) {
+                if (seconds.getTime() % 5 == 0) {
+                    System.out.println("C начала сессии прошло " + seconds.time + " секунд");
+                }
+                try {
+                    TimeUnit.MILLISECONDS.sleep(1000);
+                } catch (InterruptedException e) {
+                }
+            }
+        })).start();
+        (new Thread(()->{
+            while(seconds.getTime() < 60) {
+                if (seconds.getTime() % 7 == 0) {
+                    System.out.println("C начала сессии прошло " + seconds.time + " секунд");
+                }
+                try {
+                    TimeUnit.MILLISECONDS.sleep(1000);
+                } catch (InterruptedException e) {
+                }
+            }
+        })).start();
+    }
+}
+
